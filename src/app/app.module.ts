32bbe312e4fbd817a2441686464f6307ef76d7cf
bogdan <bogdandihtyar@gmail.com>
2018-08-04 20:47:30 +0300

    routing registration and template connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import {AppRoutingModule} from './app-routing.module';
+
 
 
 @NgModule({
@@ -14,8 +17,9 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    AuthModule
+    HttpClientModule,
+    AuthModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
