2c7dd2315111536ab6c249dcbcb3a96126b7efb9
bogdan <bogdandihtyar@gmail.com>
2018-08-04 22:40:08 +0300

    login page validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
@@ -16,7 +14,6 @@ import {AppRoutingModule} from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     AuthModule,
     AppRoutingModule
