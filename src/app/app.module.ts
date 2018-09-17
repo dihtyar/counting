7d445ba9327228278ec7bb97047decba8981b1b3
bogdan <bogdandihtyar@gmail.com>
2018-09-17 18:08:00 +0300

    page login authorisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
-import {AppRoutingModule} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import { UsersService } from './shared/services/users.service';
+import { AuthService } from './shared/services/auth.sevice';
 
 
 
@@ -14,11 +16,14 @@ import {AppRoutingModule} from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    HttpModule,
     AuthModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    UsersService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
